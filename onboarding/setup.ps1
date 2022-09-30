bca022ebb24644050692570e19deb5d010e2646a
Janne Mattila <janne.mattila@hotmail.fi>
2022-09-30 09:36:29 +0300

    Add lables to tags example

--- onboarding/setup.ps1
+++ onboarding/setup.ps1
@@ -4,8 +4,8 @@ $azure_subscription_name = "AzureDev"
 $azure_resource_group_name = "rg-defender-gcp"
 $azure_location = "westeurope"
 
-$gcp_project_id="airy-charge-364105"
-$gcp_project_number="421932273897"
+$gcp_project_id = "airy-charge-364105"
+$gcp_project_number = "421932273897"
 
 # Login and set correct contextes
 az login -o table
@@ -20,3 +20,19 @@ az group list -o table
 gcloud projects list --format="json"
 gcloud projects list --format="table(createTime.date('%Y-%m-%d'),name,projectNumber,projectId)"
 
+# Create resource group
+az group create -l $azure_location -n $azure_resource_group_name -o table
+
+# Get project labels
+$gcp_project_json = $(gcloud projects describe $gcp_project_id --format="json")
+$gcp_project_json | jq .
+
+$gcp_project_labels = $($gcp_project_json | jq .labels)
+$gcp_project_labels_json = $gcp_project_labels | ConvertFrom-Json
+$gcp_project_labels_json.company_code
+$gcp_project_labels_json.company_finance_code
+
+# Update project labels to resource group
+az group update --name $azure_resource_group_name `
+    --set tags.gcp_company_code=$($gcp_project_labels_json.company_code) `
+    --set tags.gcp_company_finance_code=$($gcp_project_labels_json.company_finance_code)
